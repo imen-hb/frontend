193632a3249da2ea3da452bce474540bae6d08b4
imen-hb <imenhbibi502@gmail.com>
2024-05-22 18:58:20 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Hero from './components/Hero/Hero';
+import Join from './components/Join/Join';
+import Plans from './components/Plans/Plans';
+import Reasons from './components/Reasons/Reasons';
+import Testimonials from './components/Testimonials/Testimonials';
+import Programs from './components/programs/Programs';
+
 function App() {
   return (
     <div className="App">
-          <span style={{color: 'white'}}>Subscribe Zainkeepscode</span>
+        <Hero/> 
+        <Programs/>
+        <Reasons/>
+        <Plans/>
+        <Testimonials/>
+        <Join/>
+        <Footer/>
     </div>
   );
 }
